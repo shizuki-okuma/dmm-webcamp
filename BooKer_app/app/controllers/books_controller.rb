669c4e89619a5e57a65e76f7600127e57f83a9a8
EC2 Default User <126657775+shizuki-okuma@users.noreply.github.com>
2023-03-12 08:14:49 +0000

    [BooKer Finish]

--- BooKer_app/app/controllers/books_controller.rb
+++ BooKer_app/app/controllers/books_controller.rb
@@ -18,17 +18,25 @@ class BooksController < ApplicationController
   def show
     @book = Book.find(params[:id])  
     @books = Book.all
+  
+    
   end
 
   def edit
+  
     @book = Book.find(params[:id])
   end
   
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)  
+    @book = Book.find(params[:id])
+  if @book.update(book_params)
+    flash[:notice] = "Book was successfully updated."
+    redirect_to book_path(@book.id) 
+  else
+    render :edit
   end
+  end
+  
   def destroy
     book = Book.find(params[:id])
     book.destroy
